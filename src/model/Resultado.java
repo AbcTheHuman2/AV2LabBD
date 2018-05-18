6c3caab043a0b51d491c114d044a290ee0b691d3
AbcTheHuman2 <athehuman@gmail.com>
2018-05-17 22:37:37 -0300

    Classe Resultado.java criada

---
+++ src/model/Resultado.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Resultado {
+	
+	private String atleta_nome;
+	private String pais_nome;
+	private float desempenho;
+	
+	public String getAtletaNome() {
+		return atleta_nome;
+	}
+	
+	public void setAtletaNome(String atleta_nome) {
+		this.atleta_nome = atleta_nome;
+	}
+	
+	public String getPaisNome() {
+		return pais_nome;
+	}
+	
+	public void setPaisNome(String pais_nome) {
+		this.pais_nome = pais_nome;
+	}
+	
+	public float getDesempenho() {
+		return desempenho;
+	}
+	
+	public void setDesempenho(float desempenho) {
+		this.desempenho = desempenho;
+	}
+}
